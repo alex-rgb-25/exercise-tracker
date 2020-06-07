45bc7083f4bdfd88fa62b931ae867df56957f55a
Alexandru Simionescu <alexandru.simionescu.94@gmail.com>
2020-06-07 14:21:03 +0300

    saved

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ var Schema = mongoose.Schema;
 var exerciseSchema = new Schema({
     description: String,
     duration: String,
-    date: String
+    date: Date
 })
 var Exercise = mongoose.model("Exercise", exerciseSchema);
 
@@ -55,8 +55,6 @@ var usersSchema=new Schema({
 //define the model:
 
 var User = mongoose.model('User', usersSchema);
-
-
 
 app.get("/", function(req, res){
     res.render("landing");
@@ -91,17 +89,11 @@ app.post("/api/exercise/add", function(req, res){
     if(req.body.date==""){
         var d = new Date();
     }else{ var d= new Date(req.body.date)}
-    var date = d.getDate();
-    var month = d.getMonth() + 1; // Since getMonth() returns month from 0-11 not 1-12
-    var year = d.getFullYear();
-    var dateStr = date + "-" + month + "-" + year;
-    
-
     User.findOne({_id:req.body.userID}, function(err, found){
         if(err){ return handleError(err)}
         else{
             Exercise.create({ description: req.body.description,
-                duration: req.body.duration, date: dateStr}, function(err, newExercise){
+                duration: req.body.duration, date: d}, function(err, newExercise){
                     if(err){ return handleError(err)}
                     else{
                         found.exercise.push(newExercise)
@@ -113,22 +105,45 @@ app.post("/api/exercise/add", function(req, res){
     })
 })
 
-/*User.create({username:'alex', password:'l'}, function(err, user){
-    if(err){ console.log(err)}
-    else{
-        Exercise.create({
-            description:'running', duration:'30',
-            reps:3, date:'11-11-2020'
-        }, function(err, exercise){
-            if(err){ console.log(err)}
+app.get("/api/exercise/log/:userID/:from?/:to?/:limit?", function(req, res){
+    console.log(req.params.userID)
+    var count = 0;
+    var arr =[]
+    if(req.params.from!=null  && req.params.to!=null & req.params.limit!=null){
+        console.log("not null")
+
+        User.findOne({_id: req.params.userID}, function(err, found){
+            if(err){ handleError(err)}
             else{
-                user.exercise.push(exercise);
-                user.exercise.push(exercise);
-                user.save();
-            }
-        })
+                let results = found.exercise;
+                console.log("FOUND.EXERCISES !!! :  " + results)
+                let fromDate = new Date(req.params.from);
+                let toDate = new Date(req.params.to);
+                let limit = Number(req.params.limit);
+
+
+                    Exercise.find({_id: {$in: results}}, function(err, foundEx){
+                        if(err){ handleError(err)}
+                        else{
+                        console.log(foundEx)
+                        //var final= foundEx.filter((ex) => ex.date >= fromDate && ex.date <= toDate)
+
+                       var final= foundEx.filter(function(ex){
+                           
+                           return ex.date< toDate && ex.date>=fromDate
+                       })
+                       res.send({"exercise":final});
+                        }
+                        console.log("here is filtered: " + final);
+                    })
+                
+                }
+            })
+        } else{
+        console.log("is null")
     }
-})*/
+   
+})
 
 app.listen(process.env.PORT || 3000, function(req, res){
     console.log("Server started!");
